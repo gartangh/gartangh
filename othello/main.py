a421b0f0d1430846d1cda7f49afd718bc42b8990
OlivierVdN <olivier.vandennest@ugent.be>
2020-04-23 19:15:21 +0200

    mix training + dqn
    converted spaces back to tabs
    
    converted spaces back into tabs

--- othello/main.py
+++ othello/main.py
@@ -6,6 +6,7 @@ from tqdm import tqdm
 from game_logic.agents.cnn_dqn_trainable_agent import CNNDQNTrainableAgent
 from game_logic.agents.dqn_trainable_agent import DQNTrainableAgent
 from game_logic.agents.human_agent import HumanAgent
+from game_logic.agents.minimax_agent import MinimaxAgent
 from game_logic.agents.random_agent import RandomAgent
 from game_logic.agents.risk_regions_agent import RiskRegionsAgent
 from game_logic.agents.trainable_agent import TrainableAgent
@@ -24,7 +25,9 @@ def main() -> None:
 	# agents
 	black = config.black
 	white = config.white
-	print(f'\nAgents:\n\t{black}\n\t{white}\n')
+	if config.train_all_agents:
+		white = 'Rotatable'
+	print(f'\nAgents:\n\tBlack:\t{black}\n\tWhite:\t{white}\n')
 
 	win_rates = [0.0]
 	if isinstance(black, DQNTrainableAgent):
@@ -39,27 +42,62 @@ def main() -> None:
 		plt.ylabel('win ratio and epsilon')
 		plt.show()
 
+	if config.train_all_agents:
+		agents_rotation = {
+			1: MinimaxAgent(Color.WHITE,
+			                immediate_reward=MinimaxHeuristic(global_config.board_size),
+			                depth=1),
+			2: RandomAgent(Color.WHITE),
+			3: MinimaxAgent(Color.WHITE,
+			                immediate_reward=MinimaxHeuristic(global_config.board_size),
+			                depth=1),
+			4: RiskRegionsAgent(Color.WHITE, global_config.board_size),
+			5: MinimaxAgent(Color.WHITE,
+			                immediate_reward=MinimaxHeuristic(global_config.board_size),
+			                depth=1),
+		}
+		black_wins = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+		rotation_num_episodes = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+		assert len(agents_rotation) == len(black_wins)
+		assert len(agents_rotation) == len(rotation_num_episodes)
+
 	for episode in tqdm(range(1, config.num_episodes + 1)):
+		if config.train_all_agents:
+			opponent = agents_rotation[1 + episode % len(agents_rotation)]
+			config.white = opponent
+
 		# create new game
 		game: Game = Game(global_config, config, episode)
 		if isinstance(white, HumanAgent):
 			# create GUI controller
 			controller: Controller = Controller(game)
 			controller.start()
+		elif isinstance(black, DQNTrainableAgent):
+			# Update epsilon annealing policy
+			black.training_policy.update_policy(episode)
+			# play game
+			game.play()
 		else:
 			# play game
 			game.play()
+
+		if config.train_all_agents:
+			rotation_num_episodes[1 + episode % len(agents_rotation)] += 1
 
 		# plot win ratio
 		if config.plot_win_ratio:
 			if game.board.num_black_disks > game.board.num_white_disks:
 				last_matches.append(1)
+				if config.train_all_agents:
+					black_wins[1 + episode % len(agents_rotation)] += 1
 			elif game.board.num_black_disks < game.board.num_white_disks:
 				last_matches.append(-1)
 			else:
 				last_matches.append(0)
 
-			if episode % config.plot_every_n_episodes == config.plot_every_n_episodes - 1 and len(last_matches) > 0:
+			if ((episode - 1) % 100 == 0 or (
+					episode % config.plot_every_n_episodes == config.plot_every_n_episodes - 1)) and len(
+				last_matches) > 0:
 				win_rates.append(sum(last_matches) / len(last_matches))
 				if isinstance(black, DQNTrainableAgent):
 					epsilons.append(black.training_policy.current_eps_value)
@@ -73,20 +111,16 @@ def main() -> None:
 					plt.pause(0.001)
 				last_matches = []
 
+	print()
+	print()
+
 	# print end score
-	ties: int = config.num_episodes - black.num_games_won - white.num_games_won
-	if black.num_games_won > white.num_games_won:
-		print(colored(
-			f'\nBLACK {black.num_games_won:>5}/{config.num_episodes:>5} ({black.num_games_won:>5}|{white.num_games_won:>5}|{ties:>5})\n',
-			'red'))
-	elif black.num_games_won < white.num_games_won:
-		print(colored(
-			f'\nWHITE {white.num_games_won:>5}/{config.num_episodes:>5} ({black.num_games_won:>5}|{white.num_games_won:>5}|{ties:>5})\n',
-			'green'))
+	if config.train_all_agents:
+		for key in agents_rotation:
+			print(f'White: {agents_rotation[key]}')
+			print_scores(rotation_num_episodes[key], black_wins[key], agents_rotation[key].num_games_won)
 	else:
-		print(colored(
-			f'\nDRAW  {black.num_games_won:>5}/{config.num_episodes:>5} ({black.num_games_won:>5}|{white.num_games_won:>5}|{ties:>5})\n',
-			'cyan'))
+		print_scores(config.num_episodes, black.num_games_won, white.num_games_won)
 
 	# plot win ratio
 	if config.plot_win_ratio_live:
@@ -109,6 +143,22 @@ def main() -> None:
 		black.final_save()
 	if isinstance(white, TrainableAgent) and white.train_mode:
 		white.final_save()
+
+
+def print_scores(num_episodes: int, black_won: int, white_won: int):
+	ties: int = num_episodes - black_won - white_won
+	if black_won > white_won:
+		print(colored(
+			f'\nBLACK {black_won:>5}/{num_episodes:>5} ({black_won:>5}|{white_won:>5}|{ties:>5})\n',
+			'red'))
+	elif black_won < white_won:
+		print(colored(
+			f'\nWHITE {white_won:>5}/{num_episodes:>5} ({black_won:>5}|{white_won:>5}|{ties:>5})\n',
+			'green'))
+	else:
+		print(colored(
+			f'\nDRAW  {black_won:>5}/{num_episodes:>5} ({black_won:>5}|{white_won:>5}|{ties:>5})\n',
+			'cyan'))
 
 
 def log(logline: str, path: str = 'log.txt'):
@@ -144,7 +194,7 @@ def hardcore_training(black, white, board_size, total_iterations: int = 100_000,
 		black_dqn = black
 		white_dqn = white
 
-		tournament_mode = True
+		random_start = True
 
 		if isinstance(black, TrainableAgent):
 			# test against random white
@@ -154,7 +204,7 @@ def hardcore_training(black, white, board_size, total_iterations: int = 100_000,
 			num_episodes = 244
 			white = RandomAgent(color=Color.WHITE)
 			# TODO: use configs
-			# main(num_episodes, black, white, board_size, False, tournament_mode, False)
+			# main(num_episodes, black, white, board_size, False, random_start, False)
 			log('test ' + str(i) + '\tBLACK DQN VS WHITE RANDOM\t' + str(black.num_games_won) + '\t' + str(
 				white.num_games_won))
 
@@ -168,7 +218,7 @@ def hardcore_training(black, white, board_size, total_iterations: int = 100_000,
 			num_episodes = 244
 			black = RandomAgent(color=Color.BLACK)
 			# TODO: use configs
-			# main(num_episodes, black, white, board_size, False, tournament_mode, False)
+			# main(num_episodes, black, white, board_size, False, random_start, False)
 			log('test ' + str(i) + '\tBLACK RANDOM VS WHITE DQN\t' + str(black.num_games_won) + '\t' + str(
 				white.num_games_won))
 
@@ -182,7 +232,7 @@ def hardcore_training(black, white, board_size, total_iterations: int = 100_000,
 			num_episodes = 244
 			white = RiskRegionsAgent(color=Color.WHITE)
 			# TODO: use configs
-			# main(num_episodes, black, white, board_size, False, tournament_mode, False)
+			# main(num_episodes, black, white, board_size, False, random_start, False)
 			log('test ' + str(i) + '\tBLACK DQN VS WHITE RISK_REGION\t' + str(black.num_games_won) + '\t' + str(
 				white.num_games_won))
 
@@ -196,7 +246,7 @@ def hardcore_training(black, white, board_size, total_iterations: int = 100_000,
 			num_episodes = 244
 			black = RiskRegionsAgent(color=Color.BLACK)
 			# TODO: use configs
-			# main(num_episodes, black, white, board_size, False, tournament_mode, False)
+			# main(num_episodes, black, white, board_size, False, random_start, False)
 			log('test ' + str(i) + '\tBLACK RISK_REGION VS WHITE DQN\t' + str(black.num_games_won) + '\t' + str(
 				white.num_games_won))
 
@@ -210,17 +260,22 @@ if __name__ == '__main__':
 		black=CNNDQNTrainableAgent(
 			Color.BLACK,
 			immediate_reward=MinimaxHeuristic(global_config.board_size),
-			board_size=global_config.board_size
+			board_size=global_config.board_size,
+			start_epsilon=0.99,
+			end_epsilon=0.01,
+			epsilon_steps=20_000,
+			allow_exploration=True
 		),
 		train_black=True,
 		white=RandomAgent(Color.WHITE),
 		train_white=False,
-		num_episodes=1,
-		plot_win_ratio=False,
-		plot_win_ratio_live=False,
-		verbose=True,
-		verbose_live=True,
+		num_episodes=150,
+		plot_win_ratio=True,
+		plot_win_ratio_live=True,
+		verbose=False,
+		verbose_live=False,
 		random_start=True,
+		train_all_agents=True,
 	)
 	main()
 
@@ -228,13 +283,14 @@ if __name__ == '__main__':
 	config: Config = Config(
 		black=config.black,
 		train_black=False,
-		white=config.white,
+		white=RandomAgent(Color.WHITE),
 		train_white=False,
 		num_episodes=100,
 		plot_win_ratio=False,
 		plot_win_ratio_live=False,
 		verbose=True,
 		verbose_live=False,
+		train_all_agents=False,
 		random_start=False,
 	)
 	main()
@@ -250,6 +306,7 @@ if __name__ == '__main__':
 		plot_win_ratio_live=False,
 		verbose=True,
 		verbose_live=False,
+		train_all_agents=False,
 		random_start=False,
 	)
 	main()
--- othello/utils/config.py
+++ othello/utils/config.py
@@ -8,7 +8,8 @@ from utils.color import Color
 class Config:
 	def __init__(self, black: Agent, train_black: bool, white: Agent, train_white: bool, num_episodes: int,
 	             plot_win_ratio: bool = True, plot_win_ratio_live: bool = False, verbose: bool = False,
-	             verbose_live: bool = False, random_start: bool = False) -> None:
+	             verbose_live: bool = False, random_start: bool = False,
+	             train_all_agents: bool = False) -> None:
 		# check parameters
 		assert black.color is Color.BLACK, f'Invalid black agent: black agent\'s color is not black'
 		if not isinstance(black, TrainableAgent):
@@ -16,7 +17,7 @@ class Config:
 		assert white.color is Color.WHITE, f'Invalid white agent: white agent\'s color is not white'
 		if not isinstance(white, TrainableAgent):
 			assert not train_white, f'Invalid white agent: white agent is not trainable'
-		assert 0 <= num_episodes <= 10000000, f'Invalid number of episodes: num_episodes should be between 0 and 10000, but got {num_episodes}'
+		assert 0 <= num_episodes <= 10000, f'Invalid number of episodes: num_episodes should be between 0 and 10000, but got {num_episodes}'
 		if not isinstance(black, TrainableAgent):
 			assert not plot_win_ratio, f'Cannot plot win ratio if black agent is not trainable'
 		if not plot_win_ratio:
@@ -34,6 +35,7 @@ class Config:
 		self.verbose: bool = verbose
 		self.verbose_live: bool = verbose_live
 		self.random_start: bool = random_start
+		self.train_all_agents: bool = train_all_agents
 
 		self.plot_every_n_episodes: int = ceil(self.num_episodes / 100)
 
